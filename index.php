978fdd212b6d7a088d203562d7aaa8ae1733bb81
Thiago Kasper de Souza <thiagokaspersouza@gmail.com>
2024-12-10 19:20:01 -0300

    feat: Implements hero with toggle bg on login.php

--- index.php
+++ index.php
@@ -6,10 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="./assets/js/color-modes.js"></script>
+    <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <?php include './pages/'. $page. '.php'; ?>
+    <?php include './pages/dropdown.php'; ?>
+    
+    <main>
+        <?php include './pages/'. $page. '.php'; ?>
+    </main>
+   
+    <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,24 @@
-<form method="post">
+<div class="b-example-divider mb-0"></div>
+
+<div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Border hero with cropped image and shadows</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+            <form method="post">
+                <button class="btn btn-primary" type="submit" name="form_button">Continuar com Google</button>
+            </form> 
+        </div>
+        </div>
+        <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <img class="rounded-lg-3" src="./assets/bootstrap-themes.png" alt="" width="720">
+        </div>
+    </div>
+</div>
+
+  <div class="b-example-divider mb-0"></div>
+
+<!-- <form method="post">
     <button class="btn btn-primary" type="submit" name="form_button">Continuar com Google</button>
-</form>
+</form>  -->
